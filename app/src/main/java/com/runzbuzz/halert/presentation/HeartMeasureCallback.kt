2d2a60a7612b2a8919abc9a2034a5aa596c2b4f8
Ahmad Alobaid <ahmad88me@gmail.com>
2023-05-07 21:35:49 +0300

    save

---
+++ app/src/main/java/com/runzbuzz/halert/presentation/HeartMeasureCallback.kt
@@ -0,0 +1,41 @@
+package com.runzbuzz.halert.presentation
+
+import android.util.Log
+import androidx.health.services.client.MeasureCallback
+import androidx.health.services.client.data.Availability
+import androidx.health.services.client.data.DataPointContainer
+import androidx.health.services.client.data.DataType
+import androidx.health.services.client.data.DataTypeAvailability
+import androidx.health.services.client.data.DeltaDataType
+
+class HeartMeasureCallback: MeasureCallback {
+    override fun onAvailabilityChanged(dataType: DeltaDataType<*, *>, availability: Availability) {
+        if (availability is DataTypeAvailability) {
+            Log.d("Health", "Availability is changed")
+            Log.d("Health", availability.toString())
+
+            // Handle availability change.
+        }
+    }
+
+
+
+    override fun onDataReceived(data: DataPointContainer) {
+        // Inspect data points.
+        Log.d("Health", "Data is received")
+        Log.d("Health", data.toString())
+        Log.d("Health", data.getData(DataType.HEART_RATE_BPM).last().toString())
+        Log.d("Health", data.getData(DataType.HEART_RATE_BPM).last().dataType.toString())
+        Log.d("Health", data.getData(DataType.HEART_RATE_BPM).last().value.toString())
+    }
+
+    override fun onRegistered() {
+        super.onRegistered()
+        Log.d("Health", "Registered")
+    }
+
+    override fun onRegistrationFailed(throwable: Throwable) {
+        super.onRegistrationFailed(throwable)
+        Log.d("Health", "Registration failed")
+    }
+}
